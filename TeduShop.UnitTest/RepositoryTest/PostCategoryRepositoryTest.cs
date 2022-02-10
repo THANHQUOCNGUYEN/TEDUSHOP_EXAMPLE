faecf9951b70d6fcc6707594622020f14d7bfd51
DESKTOP-0S6PRTJ\DELL <ngtquoc163@gmail.com>
2022-02-10 17:36:19 +0700

    đã chỉnh sửa một số nội dung

---
+++ TeduShop.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+
+namespace TeduShop.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        IDbFactory dbFactory;
+        IPostCategoryRepository objRepository;
+        IUnitOfWork unitOfWork;
+
+        [TestInitialize]
+        //Phuong thuc khoi tao //riêng biệt không dùng denpendientcy
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            objRepository = new PostCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory category = new PostCategory();
+            category.Name = "Test category";
+            category.Alias = "Test category";
+
+            category.Status = true;
+
+            var result = objRepository.Add(category);
+            unitOfWork.Commit();
+
+            //hai phương thức này không bị lỗi thì nó mới pass được
+            Assert.IsNotNull(result);
+            Assert.AreEqual(11, result.ID);
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_GetAll()
+        {
+            var list = objRepository.GetAll().ToList();
+            Assert.AreEqual(3, list.Count);
+        }
+    }
+}
---
+++ TeduShop.UnitTest/ServiceTest/PostCategoryServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+using TeduShop.Service;
+
+namespace TeduShop.UnitTest.ServiceTest
+{
+    //Test service thì ta dùng moq
+    [TestClass]
+    public class PostCategoryServiceTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+        [TestMethod]
+        public void PostCategory_Service_GetAll()
+        {
+
+        }
+
+    }
+}
